65b53164653ed838d13c3629d9b23b032bfbea5b
Igor Korzhan <igor.human@gmail.com>
2024-04-30 22:58:13 +0300

    Завдання №4

---
+++ Task_04/Article.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04
+{
+    public class Article
+    {
+        private string productName;
+        private string storeName;
+        private decimal priceInUAH;
+
+        public Article(string productName, string storeName, decimal priceInUAH)
+        {
+            this.productName = productName;
+            this.storeName = storeName;
+            this.priceInUAH = priceInUAH;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Назва товару: {productName}");
+            Console.WriteLine($"Назва магазину: {storeName}");
+            Console.WriteLine($"Вартість товару: {priceInUAH}");
+        }
+
+        public string GetProductName()
+        {
+            return productName;
+        }
+    }
+    public class Store
+    {
+        private Article[] articles;
+
+        public Store(Article[] articles)
+        {
+            this.articles = articles;
+        }
+
+        public void DisplayInfo(int index) //Вивод інфи по індексу
+        {
+            if (index >= 0 && index < articles.Length)
+            {
+                articles[index].DisplayInfo();
+            }
+            else
+            {
+                Console.WriteLine("Індекс виходить за межі масиву");
+            }
+        }
+
+        public void FindAticleByName(string productName)
+        {
+            bool found = false;
+
+            foreach (Article article in articles)
+            {
+                if (article.GetProductName().Equals(productName, StringComparison.OrdinalIgnoreCase))//порівнюємо рядки без урахування регістру
+                {
+                    article.DisplayInfo();
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine($"Товар з назвою '{productName}' не знайдено");
+            }
+
+        }
+    }
+}
---
+++ Task_04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using Task_04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Article[] articles = new Article[]
+        {
+            new Article("Книга", "Книгарня", 250),
+            new Article("Ноут", "ОЛХ", 15000),
+            new Article("Смартфон", "Алло", 8000),
+        };
+
+        Store store = new Store(articles);
+
+        Console.WriteLine("Інформація про товар за індексом 1:");
+        store.DisplayInfo(1);
+
+        Console.WriteLine("\nПошук товару за назвою 'Смартфон':");
+        store.FindAticleByName("Смартфон");
+    }
+}
